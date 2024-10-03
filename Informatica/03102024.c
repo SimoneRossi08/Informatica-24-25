6a6db4c2d57de2634cb1c54de2837b941d625b3e
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2024-10-03 06:51:56 +0000

    Esercizio del 03/10/2024

---
+++ Informatica/03102024.c
@@ -0,0 +1,72 @@
+
+#include <stdio.h>
+
+int main(){
+
+    int n1, n2, n3, n4, n5, t;
+    printf("Inserisci il primo valore:");
+    scanf("%d", &n1);
+    printf("Inserisci il secondo valore:");
+    scanf("%d", &n2);
+    printf("Inserisci il terzo valore:");
+    scanf("%d", &n3);
+    printf("Inserisci il quarto valore:");
+    scanf("%d", &n4);
+    printf("Inserisci il quinto valore: ");
+    scanf("%d", &n5);
+
+    if(n2<n1){
+        t=n1;
+        n1=n2;
+        n2=t;
+    }
+    if(n3<n1){
+        t=n1;
+        n1=n3;
+        n3=t;  
+    }
+    if(n4<n1){
+        t=n1;
+        n1=n4;
+        n4=t;
+    }
+    if(n3<n2){
+        t=n2;
+        n2=n3;
+        n3=t;
+    }
+    if(n4<n2){
+        t=n2;
+        n2=n4;
+        n4=t;
+    }
+    if(n4<n3){
+        t=n3;
+        n3=n4;
+        n4=t;
+    }
+    if(n5<n1){
+        t=n1;
+        n1=n5;
+        n5=t;
+    }
+    if(n5<n2){
+        t=n2;
+        n2=n5;
+        n5=t;
+    }
+    if(n5<n3){
+        t=n3;
+        n3=n5;
+        n5=t;
+    }
+    if(n5<n4){
+        t=n4;
+        n4=n5;
+        n5=t;
+    }
+    printf("i 4 valori in ordine crescente sono: %d %d %d %d %d", n1, n2, n3, n4, n5);
+
+return 0;
+
+}
