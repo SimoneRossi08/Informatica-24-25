56d82707fde177c93e2573bac3bd17aef993728a
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2024-10-14 11:07:41 +0000

    Bug Fixes

--- Informatica/01_13102024_simonerossi.c
+++ Informatica/01_13102024_simonerossi.c
@@ -5,7 +5,7 @@ Esempio:  tra la data 5/07/2024 e  14/07/2024  la più recente è il 14/07/2024.
 int main()
 {
 
-    int g1,m1,a1,g2,m2,a2;
+    int g1,m1,a1,g2,m2,a2,b;
 
         printf ("Inserisci il giorno, il mese e l'anno della prima data\n");
         scanf ("%d %d %d", &g1, &m1, &a1);
@@ -14,8 +14,49 @@ int main()
         scanf ("%d %d %d", &g2, &m2, &a2);
 
 
-        if(m1>12 || g1>31 || m2>12 || g2>31)
-                printf("I gli orari non sono accettabili");
+if(a1%100==0){
+        if(a1%400==0){
+            printf("L'anno è bisestile");
+            b=1;
+        }
+    }
+    else{
+        if(a1%4==0){
+            printf("L'anno è bisestile");
+            b=1;
+        }
+    }
+    if(m1>=1 && m1<=12){
+        if(m1==2){
+            if(g1>=1 && g1<=28+b){
+                printf("\nLa data è accettabile");
+            }
+            else{
+                printf("\nLa data non è accettabile");
+            }
+        }
+        else{
+            if(m1==11 || m1==4 || m1==6 || m1==9){
+                if(g1>=1 && g1<=30){
+                    printf("\nLa data è accettabile");
+                }
+                else{
+                    printf("\nLa data non è accettabile");
+                }
+            }
+            else{
+                if(g1>=1 && g1<=31){
+                    printf("\nLa data è accettabile");
+                }
+                else{
+                    printf("\nLa data non è accettabile");
+                }
+            }
+        }   
+    }
+    else{
+        printf("\nLa data non è accettabile");
+    }
 
         if(a1>a2)
                 printf("La prima data è la più recente\n");
