85c33d96eeec0b0260597ee71e2e96e8444639a8
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2024-10-22 16:03:20 +0200

    Bug Fixes

---
+++ 22102024_calcolatrice_switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+int main(){
+    int num1=0;
+    int num2=0;
+    char operazione;
+
+    printf(" inserisci il primo numero: ");
+    scanf("%d", &num1);
+    printf("inserisci il secondo numero: ");
+    scanf("%d", &num2);
+    printf("quale operazione vuoi fare:");
+    scanf("%c", &operazione);
+    switch(operazione){
+        case 'a': 
+
+        case 'm': 
+        case 's':
+        case 'd':
+
+        printf("")
+    }
+    
+    
+    
+
+}
