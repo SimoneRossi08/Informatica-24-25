acf87b56001c91f022ed46d7c0e927f55c11df99
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-02-03 10:53:31 +0000

    Lezione del 03/02/2025

---
+++ Materie/Informatica/Esercizi/Lezioni/Funzioni/conversionedecbin.c
@@ -0,0 +1,36 @@
+/* Dato un valore eseguire la conversione da decimale a binario. */
+
+#include <stdio.h>
+#include <math.h>
+
+long converti(int _n);
+
+int main(){
+
+    int n=0;
+    long conv=0;
+
+    printf("Inserisci un valore: ");
+    scanf("%d", &n);
+
+    while(n<=0){
+        conv=converti(n);
+    }
+
+}
+
+long converti(int _n){
+
+    int quoz=_n;
+    int resto=0;
+    int cont=0;
+    int _conv=0;
+
+    while(quoz!=0){
+        resto=quoz%2;
+        quoz=quoz/2;
+        _conv=pow(10,cont);
+        cont++;
+    }
+    return _conv;
+}
