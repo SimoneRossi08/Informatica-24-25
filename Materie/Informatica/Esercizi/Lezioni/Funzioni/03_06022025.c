931b5fd5af69c66c930fd5a9719f70af159edb21
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-02-06 17:58:40 +0000

    Compiti del 06/02/2025

--- Materie/Informatica/Esercizi/Lezioni/Funzioni/03_06022025.c
+++ Materie/Informatica/Esercizi/Lezioni/Funzioni/03_06022025.c
@@ -4,8 +4,8 @@
 
 #include <stdio.h>
 
-float circonferenza(*_c);
-float area(*_a);
+float circonferenza(float *_c, float _r, float _pi);
+float area(float *_a, float _r, float _pi);
 
 int main(){
 
@@ -14,9 +14,19 @@ int main(){
     printf("Inserisci il valore del raggio: ");
     scanf("%f", &r);
 
+    circonferenza(&c, r, pi);
+    printf("La circonferenza vale %.2f. \m", c);
+
+    area(&a, r, pi);
+    printf("L'area vale %.2f. \n", a);
 }
 
-float circonferenza(*_c){
+float circonferenza(float *_c, float _r, float _pi){
 
-    *_a
+    *_c=2*(_r)*(_pi);
+}
+
+float area(float *_a, float _r, float _pi){
+
+    *_a=(_pi)*(_r)*(_r);
 }
