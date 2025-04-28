81b4ad445deee3f7f85cce2cffb9a04b64e23e8f
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-04-28 09:49:40 +0000

    Lezione del 28/04/2025

---
+++ Materie/Informatica/Esercizi/Lezioni/Vettori/01_28042025.c
@@ -0,0 +1,42 @@
+/* Creiamo un menu di richieste
+    1. Caricare un array
+    2. Stampare l'array
+    3. Ordina l'array tramite BubbleSort */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "array.h"
+#include "array.c"
+#define DIM 5
+
+int main(){
+
+    int v[DIM], scelta;
+
+    do{
+        printf("\nMENU: ");
+        printf("\n1. Caricare un array. ");
+        printf("\n2. Stampare l'array. ");
+        printf("\n3. Ordina l'array tramite BubbleSort. ");
+        printf("\nDigitare 0 per terminare il programma. \n");
+        scanf(" %d", &scelta);
+
+        switch(scelta){
+            case 1:
+                riempivettore(v, DIM);
+                break;
+            case 2:
+                stampavettore(v, DIM, ' ');
+                break;
+            case 3:
+                bubblesort(v, DIM);
+                break;
+            default:
+                if(scelta!=0){
+                    printf("Opzione inesistente... ");
+                }
+                break;
+        }
+    }
+    while(scelta!=0);
+}
